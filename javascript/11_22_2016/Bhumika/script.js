6f1b1e53f5b884b26a48ade87d1d4e15175ef4e6
bhumika <bhumika.r.desai17@gmail.com>
2016-11-22 09:56:37 -0600

    Javascript Assg 1

---
+++ javascript/11_22_2016/Bhumika/script.js
@@ -0,0 +1,30 @@
+// Number
+var x = 5;
+var y = 5.2;
+
+
+// String
+var name = 'Bhumika';
+
+
+// Boolean
+var isReading = false;
+var isWriting = true;
+
+
+// Null
+var a = null;
+//var b= '';
+
+
+// Undefined
+var c;
+
+console.log(name);
+console.log(x);
+console.log(y);
+console.log(isReading);
+console.log(isWriting);
+console.log(a);
+console.log(c);
+
