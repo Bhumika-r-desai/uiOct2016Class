1a89249f602cbe41d16d266bc1fce631abcdaaf8
bhumika <bhumika.r.desai17@gmail.com>
2016-12-21 19:55:05 -0600

    Assignment

--- javascript/12_05_2016-Closures/Bhumika/bhumikaClosures.js
+++ javascript/12_05_2016-Closures/Bhumika/bhumikaClosures.js
@@ -2,35 +2,34 @@
 (function(){
 
         // total
-        function Total(){
+        function Total() {
             var total = this;
             // mathematics
-            function Mathematics(){
+            function Mathematics() {
                 var math = this;
                 math.sum = function () {
                     return (a + b);
-                }
+                };
                 math.difference = function () {
                     return (a - b);
-                }
+                };
                 math.multiply = function () {
                     return (a * b);
-                }
+                };
                 math.divide = function () {
                     return (a / b);
-                }
+                };
 
             }
+                var maths = new Mathematics(1000, 10);
 
-            var maths = new Mathematics( 20, 15);
-
-            total.add = maths.sum();
-            total.substract = maths.difference();
-            total.multiplication = maths.multiply();
-            total.division = maths.divide();
-
-            var result = new total;
-            
+                total.add = maths.sum();
+                total.substract = maths.difference();
+                total.multiplication = maths.multiply();
+                total.division = maths.divide();
         }
+
+            var result = new Total();
+            console.log(result);
 
 }) ();
